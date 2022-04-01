11bee0527d6063f7be16693168b9a79e36c5131a
Smit <smitbhansali7@gmail.com>
2022-04-01 13:40:28 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <SpeechProvider appId="6d1ab73b-8fd7-43dc-886b-06c573aac77e" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
+  document.getElementById('root'),
+);
